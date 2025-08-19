d0c4384ba6b292592e9a9882b1e7cf62a8ab31c1
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-08-19 08:31:30 -0500

    created user app

--- shopmajuu/users/models.py
+++ shopmajuu/users/models.py
@@ -8,5 +8,8 @@ class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    phone_number = models.IntegerField(max_length=10)
-    
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    town = models.CharField(max_length=100, blank=True, null=True)
+    state_province = models.CharField(max_length=100, blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)

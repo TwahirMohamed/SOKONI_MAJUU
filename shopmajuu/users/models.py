843160a1b9d0d4e9b773741948f3de74b4510733
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-08-19 07:56:58 -0500

    creqted custom user model

--- shopmajuu/users/models.py
+++ shopmajuu/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone_number = models.IntegerField(max_length=10)
+    
